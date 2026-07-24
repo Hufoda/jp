50d481a1278b2b70b159c3940001a0fcc5d16d08
Aye Khant Zay <hufoda@users.noreply.github.com>
2026-07-24 13:17:01 +0630

    Allow is_staff and is_superuser to automatically act as Teachers/Admins

--- myDjango/japan/views.py
+++ myDjango/japan/views.py
@@ -117,7 +117,8 @@ def lesson_list(request, level):
 
 
 def is_admin(user):
-    return user.is_authenticated and user.groups.filter(name="Admins").exists()
+    return user.is_authenticated and (user.is_staff or user.is_superuser or user.groups.filter(name="Admins").exists())
+
 
 @login_required
 @user_passes_test(is_admin, login_url='forbidden_page')
